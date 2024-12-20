02f4f06fb4be162f6c9aee9d88b5d30c951030df
Yevhenns <gifestum@gmail.com>
2024-12-20 14:12:42 +0200

    refactor

--- src/components/list/App-List.vue
+++ src/components/list/App-List.vue
@@ -1,71 +1,14 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
-import AppForm from '../shared/App-Form.vue';
 import AppListItem from './App-ListItem.vue';
 import AppButton from '../shared/App-Button.vue';
-import { FetchInfoProps, getInfo } from '../../api/getInfo';
-import { toast } from 'vue3-toastify';
+import { NewItem } from '../../pages/Home-Page.vue';
 
-interface NewItem {
-  number: string;
-  status: string;
-}
-const info = ref<TrackingDocument | null>(null);
-const parcelsArray = ref<NewItem[]>([]);
-const isLoading = ref(false);
-const isFormShown = ref(false);
-
-onMounted(() => getParcelsFromLS());
-
-const getParcelsFromLS = () => {
-  const storedParcels = localStorage.getItem('parcels');
-  const parcels = storedParcels ? JSON.parse(storedParcels) : [];
-  parcelsArray.value = parcels as NewItem[];
-};
-
-const showForm = () => {
-  isFormShown.value = !isFormShown.value;
-};
-
-const deleteItemFromLS = (number: string) => {
-  const filteredArray = parcelsArray.value.filter(
-    item => item.number !== number
-  );
-  parcelsArray.value = filteredArray;
-  localStorage.setItem('parcels', JSON.stringify(parcelsArray.value));
-};
-
-const setInfoData = async ({ documentNumber }: FetchInfoProps) => {
-  try {
-    isLoading.value = true;
-    const data = await getInfo({
-      documentNumber,
-    });
-    if (data) {
-      const newObject: NewItem = {
-        number: data.Number,
-        status: data.Status,
-      };
-      getParcelsFromLS();
-      if (parcelsArray.value.some(item => item.number === newObject.number)) {
-        isLoading.value = false;
-        toast.error('Посилка вже у списку', {
-          autoClose: 2000,
-        });
-        return;
-      }
-      parcelsArray.value.push(newObject);
-      localStorage.setItem('parcels', JSON.stringify(parcelsArray.value));
-    }
-    isLoading.value = false;
-  } catch (e) {
-    toast.error(e, {
-      autoClose: 2000,
-    });
-    isLoading.value = false;
-    info.value = null;
-  }
-};
+defineProps<{
+  parcelsArray: NewItem[];
+  deleteItemFromLS: (number: string) => void;
+  showForm: () => void;
+  isFormShown: boolean;
+}>();
 </script>
 
 <template>
@@ -99,12 +42,6 @@ const setInfoData = async ({ documentNumber }: FetchInfoProps) => {
       ></i>
     </AppButton>
   </div>
-  <AppForm
-    v-if="isFormShown"
-    :showPhone="false"
-    :setInfoData="setInfoData"
-    :isLoading="isLoading"
-  />
 </template>
 
 <style>
--- src/pages/Home-Page.vue
+++ src/pages/Home-Page.vue
@@ -1,10 +1,86 @@
 <script setup lang="ts">
+import { toast } from 'vue3-toastify';
 import AppList from '../components/list/App-List.vue';
+import { onMounted, ref } from 'vue';
+import { FetchInfoProps, getInfo } from '../api/getInfo';
+import AppForm from '../components/shared/App-Form.vue';
+
+export interface NewItem {
+  number: string;
+  status: string;
+}
+const info = ref<TrackingDocument | null>(null);
+const parcelsArray = ref<NewItem[]>([]);
+const isLoading = ref(false);
+const isFormShown = ref(false);
+
+onMounted(() => getParcelsFromLS());
+
+const getParcelsFromLS = () => {
+  const storedParcels = localStorage.getItem('parcels');
+  const parcels = storedParcels ? JSON.parse(storedParcels) : [];
+  parcelsArray.value = parcels as NewItem[];
+};
+
+const showForm = () => {
+  isFormShown.value = !isFormShown.value;
+};
+
+const deleteItemFromLS = (number: string) => {
+  const filteredArray = parcelsArray.value.filter(
+    item => item.number !== number
+  );
+  parcelsArray.value = filteredArray;
+  localStorage.setItem('parcels', JSON.stringify(parcelsArray.value));
+};
+
+const setInfoData = async ({ documentNumber }: FetchInfoProps) => {
+  try {
+    isLoading.value = true;
+    const data = await getInfo({
+      documentNumber,
+    });
+    if (data) {
+      const newObject: NewItem = {
+        number: data.Number,
+        status: data.Status,
+      };
+      getParcelsFromLS();
+      if (parcelsArray.value.some(item => item.number === newObject.number)) {
+        isLoading.value = false;
+        toast.error('Посилка вже у списку', {
+          autoClose: 2000,
+        });
+        return;
+      }
+      parcelsArray.value.push(newObject);
+      localStorage.setItem('parcels', JSON.stringify(parcelsArray.value));
+    }
+    isLoading.value = false;
+  } catch (e) {
+    toast.error(e, {
+      autoClose: 2000,
+    });
+    isLoading.value = false;
+    info.value = null;
+  }
+};
 </script>
 
 <template>
   <div class="page">
-    <AppList />
+    <AppList
+      :parcelsArray="parcelsArray"
+      :deleteItemFromLS="deleteItemFromLS"
+      :showForm="showForm"
+      :isFormShown="isFormShown"
+    />
+    <AppForm
+      v-if="isFormShown"
+      :showPhone="false"
+      :setInfoData="setInfoData"
+      :isLoading="isLoading"
+    />
   </div>
 </template>
 
