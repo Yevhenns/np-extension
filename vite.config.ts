d88a6a8f0787a83b8f4ca4748b196aef0ed3ac4c
Yevhenns <gifestum@gmail.com>
2024-12-24 10:58:40 +0200

    deleted unused env define

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,12 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath } from 'url';
 
 export default defineConfig(() => {
-  const env = loadEnv('', process.cwd());
-
   return {
     plugins: [vue()],
     build: {
       outDir: 'build',
-    },
-    define: {
-      API_URL: JSON.stringify(env.VITE_API_URL),
-      API_KEY: JSON.stringify(env.VITE_API_KEY),
     },
     resolve: {
       alias: [
