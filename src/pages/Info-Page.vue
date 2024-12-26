4296ce7357579ef950de704bc3f7e96676647223
Yevhenns <gifestum@gmail.com>
2024-12-26 12:30:58 +0200

    added api key settings

--- src/pages/Info-Page.vue
+++ src/pages/Info-Page.vue
@@ -3,25 +3,40 @@ import AppContainer from '@/components/layout/App-Container.vue';
 </script>
 
 <template>
-  <div class="page">
+  <div>
     <AppContainer>
-      <p>Застосунок для пошуку інформації про відправлення Нової Пошти.</p>
-      <p>
-        Використовується
-        <a
-          class="np-link"
-          href="https://developers.novaposhta.ua/"
-          target="_blank"
-          rel="noopener noreferrer"
-          >АРI компанії «Нова пошта»</a
-        >.
-      </p>
+      <h3>Інформація</h3>
+      <ul class="text-wrapper">
+        <li>
+          <p>Застосунок для пошуку інформації про відправлення Нової Пошти.</p>
+        </li>
+        <li>
+          <p>
+            На сторінці "Налаштування" є можливість керування звуком і введення
+            особистого API ключа.
+          </p>
+        </li>
+        <li>
+          <p>
+            Всі дані введені користувачем зберігаються лише в local storage,
+            та/або відправляються напряму на
+            <a
+              class="np-link"
+              href="https://developers.novaposhta.ua/"
+              target="_blank"
+              rel="noopener noreferrer"
+              >АРI компанії «Нова пошта»</a
+            >
+            для отримання актуальної інформації.
+          </p>
+        </li>
+      </ul>
     </AppContainer>
   </div>
 </template>
 
 <style scoped>
-.page {
+.text-wrapper {
   text-align: left;
 }
 
--- src/store/settings.ts
+++ src/store/settings.ts
@@ -3,10 +3,15 @@ import { useLocalStorage } from '@vueuse/core';
 
 export const useSettingsStore = defineStore('settings', () => {
   const isSoundOn = useLocalStorage<boolean>('sound', true);
+  const API_KEY = useLocalStorage<string>('API_KEY', '');
 
   function toggleIsSoundOn(value: boolean) {
     isSoundOn.value = value;
   }
 
-  return { isSoundOn, toggleIsSoundOn };
+  function setApiKey(value: string) {
+    API_KEY.value = value;
+  }
+
+  return { isSoundOn, API_KEY, toggleIsSoundOn, setApiKey };
 });
